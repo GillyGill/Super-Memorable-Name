dfd006104f2afa14ad9db0285a903a4281b0ea8e
nolanjames72vr <91799129+nolanjames72vr@users.noreply.github.com>
2021-10-10 21:19:16 -0500

    Unit tests added

---
+++ Unit-Test/UnitTest1.cs
@@ -0,0 +1,31 @@
+using System;
+using Xunit;
+
+namespace Unit_Test
+{
+   public class UnitTest1
+   {
+      [Fact]
+      public void Test1()
+      {
+         // Arrange
+         string expected = "Hello, ";
+         Project1.main test = new();
+         // Act
+         string actual = test.greeting();
+         // Assert
+         Assert.Equal(expected, actual);
+      }
+      [Fact]
+      public void Test2()
+      {
+         // Arrange
+         string expected = "Goodbye";
+         Project1.main test = new();
+         // Act
+         string actual = test.goodbye();
+         // Assert
+         Assert.Equal(expected, actual);
+      }
+   }
+}
